48df1c1d66aeb905661ee486e866170c69109f93
Leere Ewigkeit <ewileere@gmail.com>
2023-09-15 04:54:11 +0300

    Init project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Leere/Desktop/CADEX
+# Build directory: C:/Users/Leere/Desktop/CADEX/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
